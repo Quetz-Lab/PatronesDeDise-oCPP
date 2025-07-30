af0d1a15313f70b3718eef5e78a4c27ee2d7bad3
Quetz-lab <quetz.yggdrasil24@outlook.com>
2025-07-29 23:23:19 -0600

    Implementación del remapeo

--- InputHandler.h
+++ InputHandler.h
@@ -1,18 +1,30 @@
 #pragma once
 #include "Command.h"
 #include <string>
+#include <map>
+#include <memory> //Para usar std::unique_ptr
 class InputHandler
 {
+private:
+	std::map<char, std::unique_ptr<Command>> keyMappings;
+	Command* botonW_;
+	Command* botonF_;
+	Command* botonI_;
+
 public:
 	InputHandler();
 	~InputHandler();
 	Command* handleInput(char tecla);
 	
-	void remapearTecla(char telca, std::string accion);
-	
-private:
-	Command* botonW_;
-	Command* botonF_;
-	Command* botonI_;
+	void remapearTecla(char telca, std::string accion)
+	{
+		if (keyMappings.count(tecla))
+			keyMappings[tecla]->execute();
+	}
+	void bindCommand(char tecla, std::unique_ptr<Command> command)
+	{
+		keyMappings[tecla] = std::move(command);
+	}
+
 };
 
